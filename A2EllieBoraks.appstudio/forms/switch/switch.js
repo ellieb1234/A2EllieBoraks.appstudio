9c02cdf40491a537b17cf504bd89684e8245c0bf
ellieb1234 <ellieboraks@creighton.edu>
2021-02-25 17:02:31 -0600

    Create switch.js

---
+++ A2EllieBoraks.appstudio/forms/switch/switch.js
@@ -0,0 +1,26 @@
+let firstName=prompt("What is your name?")
+let state=prompt("What state are you in (2 letters)?")
+let temp=prompt("What temperature (in Farenheit) is it outside?")
+
+let messages=["you should wear a warm coat, hat, scarf and gloves.", "you should wear a warm coat but you won't need a hat, scarf or gloves.", "you should wear a light coat or hoodie.", "you do not need any outerwear."]
+
+let output=`${firstName}, in ${state} it is ${temp} degrees Farenheit and `
+
+switch (temp) {
+  case <32:
+    output+=messages[0]
+    break;
+  case <50:
+    output+=messages[1]
+    break;
+  case <70:
+    output+=messages[2]
+    break;
+  case <100:
+    output+=messages[3]
+    break;
+  default:
+    output+="it's way too hot outside!"
+}
+
+console.log(output)
