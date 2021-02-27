abbc4b3c5ec6299602c7eb26ba74c6a637bb528c
ellieb1234 <ellieboraks@creighton.edu>
2021-02-27 12:45:11 -0600

    Update switch.js

--- A2EllieBoraks.appstudio/forms/switch/switch.js
+++ A2EllieBoraks.appstudio/forms/switch/switch.js
@@ -1,11 +1,12 @@
+//declare variables
 let firstName=prompt("What is your name?")
 let state=prompt("What state are you in (2 letters)?")
 let temp=prompt("What temperature (in Farenheit) is it outside?")
 
 let messages=["you should wear a warm coat, hat, scarf and gloves.", "you should wear a warm coat but you won't need a hat, scarf or gloves.", "you should wear a light coat or hoodie.", "you do not need any outerwear."]
-
 let output=`${firstName}, in ${state} it is ${temp} degrees Farenheit and `
 
+//pick temperature message
 switch (temp) {
   case <32:
     output+=messages[0]
